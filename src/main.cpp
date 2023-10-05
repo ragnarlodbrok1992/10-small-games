ea16eba3bf1fbb3b8e7e9490a70f5179943ad5db
Ragnar Lodbrok <maraus92@gmail.com>
2023-10-05 16:59:00 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 
 #include <stdio.h>
 
+const int SCREEN_WIDTH  = 800;
+const int SCREEN_HEIGHT = 600;
+
+SDL_Window*   WINDOW = NULL;
+SDL_Renderer* RENDERER = NULL;
+
 int main() {
   printf("Small engine for 10 games.\n");
 
   printf("Last message - goodbye.\n");
   return 0;
 }
+
